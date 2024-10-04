6b69a75a156052793ff337702100a292e7a2ec76
Ariel Young <ayyoung9@gmail.com>
2024-10-04 15:51:36 -0400

    Initialize repo

---
+++ src/main/java/young/ariel/gitresource/GitResourceApplication.java
@@ -0,0 +1,13 @@
+package young.ariel.gitresource;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitResourceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GitResourceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/young/ariel/gitresource/GitResourceApplicationTests.java
@@ -0,0 +1,13 @@
+package young.ariel.gitresource;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GitResourceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
